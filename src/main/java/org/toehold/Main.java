1d8b7af1ba6e93355c87f086be312b8d89b99682
SuperRock123 <2578919767@proton.me>
2025-09-17 17:24:25 +0800

    feat: 基本项目结构

---
+++ src/main/java/org/toehold/Main.java
@@ -0,0 +1,13 @@
+package org.toehold;
+import cn.zmvision.ccm.smserver.entitys.SensorData;
+import cn.zmvision.ccm.smserver.server.SmServer;
+import cn.zmvision.ccm.smserver.service.DataService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
