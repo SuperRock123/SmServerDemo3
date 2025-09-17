0f6f9ffe342777776ee51060ec2641837451081b
SuperRock123 <2578919767@proton.me>
2025-09-17 17:24:25 +0800

    feat: 基本项目结构

---
+++ src/test/java/test1.java
@@ -0,0 +1,18 @@
+import cn.zmvision.ccm.smserver.service.DataService;
+import org.junit.jupiter.api.Test;
+import org.toehold.toeholdServerImp;
+
+public class test1 {
+    DataService service=new toeholdServerImp() ;
+
+    @Test
+    public void test1() {
+        System.out.println("test1");
+        boolean r=service.check_allow_sn("1");
+    }
+    @Test
+    public void test2() {
+        System.out.println("test2");
+
+    }
+}
